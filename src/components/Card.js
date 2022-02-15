106aa98620ba3c8124eb7f5e066e9ebd634288f8
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-02-16 02:09:27 +0530

    airbnb app new commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,10 +14,10 @@ export default function Card(props){
             
             {badgeText && <div className="card-badge">{badgeText}</div>}
             
-            <img src={`/images/${props.coverImg}`} className="card-image" />
+            <img src={`./images/${props.coverImg}`} className="card-image" />
 
             <div className="card-stats">
-                <img src="/images/star.png" className="card-star" />
+                <img src="./images/star.png" className="card-star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
