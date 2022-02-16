97ed30156e73d854a0221ba1a5557d50a29e4338
Adarsh23100 <adarshrajesh23100@gmail.com>
2022-02-16 12:43:23 +0530

    airbnb new commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar(){
     return(
         <nav>
-            <img src="./airbnb-react-website/images/airbnb-logo.png" className="nav-logo" />
+            <img src="/airbnb-react-website/images/airbnb-logo.png" className="nav-logo" />
         </nav>
     )
 }
